fda08484e3ff173be9be3c22d1465b34291fbd15
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-20 15:44:27 +0100

    feat: dati passati alla view con blade

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    
+    $title = 'App Laravel';
+
+    return view('home', compact('title'));
 });
