0b50e4e41285d460ecdb6a2e5d886063c66c17ab
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-20 15:56:15 +0100

    fix: posso vedere il titolo stampato

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'App Laravel';
+    
+    return view('home', compact('title'));
 });
